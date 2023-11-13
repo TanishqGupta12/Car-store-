479cafe1d9b26867298dcc5c25d65416e9e01494
TanishqGupta <76784859+TanishqGupta12@users.noreply.github.com>
2023-11-13 18:08:37 +0530

    adding footer and navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,33 @@
-'use client'
+import { Navbar, Footer , Product } from "../components/index.js";
 
 export default function Home() {
+  // async function api () {
 
-  async function api () {
-    const axios = require('axios');
+  //   const options = {
+  //     method: 'GET',
+  //     url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+  //     params: {model: 'corolla'},
+  //     headers: {
+  //       'X-RapidAPI-Key': '5773404b06mshedcfecac51f4cd6p1c5e5bjsnfa3624b2d41b',
+  //       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  //     }
+  //   };
 
-    const options = {
-      method: 'GET',
-      url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-      params: {model: 'corolla'},
-      headers: {
-        'X-RapidAPI-Key': '5773404b06mshedcfecac51f4cd6p1c5e5bjsnfa3624b2d41b',
-        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-      }
-    };
-    
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  //   try {
+  //     const response = await axios.request(options);
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      {api()}
-      
+    <main>
+      <Navbar />
+
+      <Product/>
+      {/* {api()} */}
+
+      <Footer />
     </main>
-  )
+  );
 }
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+"use client";
+
+import Navbar from "./navbar.jsx";
+import Footer from "./footer.jsx";
+import Product from "./Product.jsx";
+export {
+  Footer, // Uncomment this line if you want to export Footer
+  Navbar,
+  Product,
+};
